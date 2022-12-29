78ec9f8e1411dc8a3f067636ec7297312527105d
yeezze <yeeeze22@gmail.com>
2022-12-30 01:09:49 +0900

    feat: 오픈 알림 신청 페이지 구현

--- src/main/java/com/templestay/reservation/domain/temple/TempleController.java
+++ src/main/java/com/templestay/reservation/domain/temple/TempleController.java
@@ -1,6 +1,10 @@
 package com.templestay.reservation.domain.temple;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class TempleController {
@@ -13,5 +17,15 @@ public class TempleController {
 
     public void templeListInit() {
         templeService.saveAllTempleList();
+    }
+
+    /**
+     * 오픈 알림 신청 페이지
+     */
+    @GetMapping("/view/index")
+    public String applyOpenAlarm(Model model) {
+        List<Temple> templeList = templeService.getAllTemple();
+        model.addAttribute("temples", templeList);
+        return "index";
     }
 }
--- src/main/java/com/templestay/reservation/domain/temple/TempleService.java
+++ src/main/java/com/templestay/reservation/domain/temple/TempleService.java
@@ -22,4 +22,8 @@ public class TempleService {
 
         templeRepository.saveAll(templeList);
     }
+
+    public List<Temple> getAllTemple() {
+        return templeRepository.findAll();
+    }
 }
